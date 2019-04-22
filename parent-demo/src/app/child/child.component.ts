3a589fde287af1b495039cfca71da55f2c41b090
SamRapidops <47409233+SamRapidops@users.noreply.github.com>
2019-04-22 13:09:27 +0530

    Add files via upload

---
+++ parent-demo/src/app/child/child.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit , EventEmitter , Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Output() greetEvent = new EventEmitter();
+  name = 'Codeevoluton';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  callParentGreet() {
+  	this.greetEvent.emit(this.name);
+  }
+
+}
