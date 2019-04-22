12d67dfeb913ef3ae89e0913bc99b60b5cb924ab
SamRapidops <47409233+SamRapidops@users.noreply.github.com>
2019-04-22 13:10:36 +0530

    Add files via upload

---
+++ service-demo/src/app/child/child.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { InteractionService } from '../interaction.service';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  constructor(private _interactionService: InteractionService) { }
+
+  ngOnInit() {
+
+  	this._interactionService.teacherMessage$.subscribe(
+  		(message) => {
+  			if(message === 'Good morning') {
+  				alert('Good morning teacher')
+  			}
+  			else if (message === 'Well done') {
+  				alert('thank you teacher'); 
+  			}
+  		}
+  	);
+  }
+
+}
